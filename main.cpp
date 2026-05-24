4c44ed9ac182ce1235a14057369ade6d7ca56fab
13361706 <patrick060627@gmail.com>
2026-05-24 18:57:27 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,10 @@ using namespace std;
 
 const int INF = 1e9;
 
-// ==============================
-// 印出矩陣
-// ==============================
 void printMatrix(const vector<vector<int>>& D) {
     int n = D.size();
 
-    cout << "    ";
+    cout << "     ";
     for (int j = 0; j < n; j++) {
         cout << setw(5) << j + 1;
     }
@@ -32,55 +29,43 @@ void printMatrix(const vector<vector<int>>& D) {
     cout << "\n";
 }
 
-// ==============================
-// Floyd-Warshall Template
-// D <- W
-// for k = 1 to n
-//   for i = 1 to n
-//     for j = 1 to n
-//       if D[i][k] + D[k][j] < D[i][j]
-//          D[i][j] = D[i][k] + D[k][j]
-// ==============================
 vector<vector<int>> floydWarshall(vector<vector<int>> D) {
     int n = D.size();
 
-    // TODO:
-    // 使用三層迴圈完成 Floyd-Warshall
-    //
-    // 提示：
-    // for k = 0 到 n-1
-    //   for i = 0 到 n-1
-    //     for j = 0 到 n-1
-    //       若 D[i][k] 和 D[k][j] 都不是 INF
-    //       且 D[i][k] + D[k][j] < D[i][j]
-    //          更新 D[i][j]
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (D[i][k] < INF && D[k][j] < INF) {
+                    if (D[i][k] + D[k][j] < D[i][j]) {
+                        D[i][j] = D[i][k] + D[k][j];
+                    }
+                }
+            }
+        }
+    }
 
     return D;
 }
 
-// ==============================
-// 主程式
-// ==============================
 int main() {
-    int n = 6; // 節點 1~6
+    int n = 6;
 
-    // 初始化距離矩陣 W
     vector<vector<int>> W(n, vector<int>(n, INF));
 
-    // TODO:
-    // 1. 將對角線 W[i][i] 設為 0
-    //
-    // 2. 依照題目中的圖加入邊：
-    // 1 -> 2 = 3
-    // 1 -> 3 = 5
-    // 3 -> 2 = -2
-    // 2 -> 5 = 7
-    // 3 -> 4 = -5
-    // 3 -> 5 = -5
-    // 2 -> 4 = 2
-    // 4 -> 5 = 6
-    // 4 -> 6 = 10
-    // 5 -> 6 = 8
+    for (int i = 0; i < n; i++) {
+        W[i][i] = 0;
+    }
+
+    W[0][1] = 3;
+    W[0][2] = 5;
+    W[2][1] = -2;
+    W[1][4] = 7;
+    W[2][3] = -5;
+    W[2][4] = -5;
+    W[1][3] = 2;
+    W[3][4] = 6;
+    W[3][5] = 10;
+    W[4][5] = 8;
 
     cout << "Initial distance matrix W:\n";
     printMatrix(W);
